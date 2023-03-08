6ef97ceb1b1011eff020e5cc98b3e0520ea27375
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-03-08 23:38:33 +0100

    new files added

---
+++ os-module.js
@@ -0,0 +1,21 @@
+// Modules
+const os = require('os');
+
+//info about current user 
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system up time in seconds 
+console.log(`The system up time is ${os.uptime()} seconds`);
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+
+
+}
+console.log(currentOS);
+
+
